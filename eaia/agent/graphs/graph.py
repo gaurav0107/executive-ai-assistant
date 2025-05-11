85acadead7923adb2022f2f3fde7f2a975279a11
Gaurav Dubey <gauravdubey@Gauravs-Mac-mini.local>
2025-05-11 18:42:14 +0530

    reducing graphj size

--- eaia/agent/graphs/graph.py
+++ eaia/agent/graphs/graph.py
@@ -11,12 +11,12 @@ from eaia.agent.find_meeting_time import find_meeting_time
 from eaia.agent.rewrite import rewrite
 from eaia.agent.config.config import get_config
 from langchain_core.messages import ToolMessage
-from eaia.agent.human_inbox import (
-    send_message,
-    send_email_draft,
-    notify,
-    send_cal_invite,
-)
+# from eaia.agent.human_inbox import (
+#     send_message,
+#     send_email_draft,
+#     notify,
+#     send_cal_invite,
+# )
 from eaia.gmail_manager.gmail import (
     send_email,
     create_draft_email,
@@ -33,15 +33,15 @@ from eaia.schemas import (
 
 def route_after_triage(
     state: State,
-) -> Literal["draft_response", "label_skip", "label_notify", "label_unsure"]:
+) -> Literal["draft_response", "skip", "notify", "unsure"]:
     if state["triage"].response == "email":
         return "draft_response"
     elif state["triage"].response == "no":
-        return "label_skip"
+        return "skip"
     elif state["triage"].response == "notify":
-        return "label_notify"
+        return "notify"
     elif state["triage"].response == "unsure":
-        return "label_unsure"   
+        return "unsure"   
     else:
         raise ValueError
 
@@ -62,6 +62,21 @@ def create_email_draft(state, config):
         addn_receipients=new_receipients,
     )
 
+
+def skip_node(state):
+    add_labels_to_email(state["config"]["user_email_id"], state["email"]["id"], ["EA-SKIP"])
+    mark_as_read(state["config"]["user_email_id"], state["email"]["id"])
+    pass
+
+def notify_node(state):
+    add_labels_to_email(state["config"]["user_email_id"], state["email"]["id"], ["EA-NOTIFY"])
+    mark_as_read(state["config"]["user_email_id"], state["email"]["id"])
+    pass
+
+def unsure_node(state):
+    add_labels_to_email(state["config"]["user_email_id"], state["email"]["id"], ["EA-UNSURE"])
+    mark_as_read(state["config"]["user_email_id"], state["email"]["id"])
+    pass
 
 
 def mark_as_read_node(state):
@@ -104,21 +119,14 @@ class ConfigSchema(TypedDict):
 
 graph_builder = StateGraph(State, config_schema=ConfigSchema)
 graph_builder.add_node(triage_input)
-graph_builder.add_node(label_notify)
-graph_builder.add_node(label_unsure)
-graph_builder.add_node(label_skip)
-graph_builder.add_node(add_ea_to_thread_node)
-graph_builder.add_node(mark_as_read_node)
-graph_builder.add_node(draft_response)
-graph_builder.add_node(create_email_draft)
+graph_builder.add_node("notify", notify_node)
+graph_builder.add_node("skip", skip_node)
+graph_builder.add_node("unsure", unsure_node)
+graph_builder.add_node("draft_response", create_email_draft)
 graph_builder.set_entry_point("triage_input")
 graph_builder.add_conditional_edges("triage_input", route_after_triage)
-graph_builder.add_edge("draft_response", "create_email_draft")
-graph_builder.add_edge("create_email_draft", END)
-graph_builder.add_edge("label_notify", "add_ea_to_thread_node")
-graph_builder.add_edge("label_skip", "mark_as_read_node")
-graph_builder.add_edge("add_ea_to_thread_node", "mark_as_read_node")
-graph_builder.add_edge("label_unsure", "add_ea_to_thread_node")
-graph_builder.add_edge("add_ea_to_thread_node", "mark_as_read_node")
-graph_builder.add_edge("mark_as_read_node", END)
+graph_builder.add_edge("draft_response", END)
+graph_builder.add_edge("notify", END)
+graph_builder.add_edge("skip", END)
+graph_builder.add_edge("unsure", END)
 graph = graph_builder.compile()
